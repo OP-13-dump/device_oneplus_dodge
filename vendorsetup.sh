d0f02d9507c666afe0fe06c730219f9bfd8f5e02
NoCache-69 <chethangowdasj86@gmail.com>
2026-07-22 01:54:01 +0530

    dodge: Init vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+git clone -b 16.2 https://github.com/OP-13-dump/device_oneplus_sm8750-common device/oneplus/sm8750-common
+git clone -b 16.2 https://github.com/OP-13-dump/vendor_oneplus_dodge vendor/oneplus/dodge
+git clone -b 16.2 https://github.com/OP-13-dump/vendor_oneplus_sm8750-common vendor/oneplus/sm8750-common
+git clone -b 16.2 https://github.com/OP-13-dump/kernel_oneplus_sm8750 kernel/oneplus/sm8750 --depth=1
+git clone -b 16.2 https://github.com/OP-13-dump/kernel_oneplus_sm8750-modules kernel/oneplus/sm8750-modules
+git clone -b 16.2 https://github.com/OP-13-dump/kernel_oneplus_sm8750-devicetrees kernel/oneplus/sm8750-devicetrees
+git clone -b 16.2 https://github.com/OP-13-dump/hardware_oplus hardware/oplus
+git clone -b 16.2 https://github.com/OP-13-dump/vendor_oneplus_ir vendor/oneplus/ir
+git clone -b 16.2 https://github.com/Lunaris-AOSP/vendor_lunaris-priv_keys.git vendor/lunaris-priv/keys
