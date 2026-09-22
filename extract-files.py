AlphaDroid-devices/android_device_oneplus_dodge

dodge: Keep the HEIF switch off across camera config extracts
The blob value is rewritten on every extract, so clear
photocodec.support alongside the 120fps tags.

extract-files.py
@@ -125,6 +125,11 @@ def set_video_dv_120fps_support(ctx, file, file_path, *args, **kwargs):
 def set_camera_capture_hdr_support(ctx, file, file_path, *args, **kwargs):
     update_vendor_tag(ctx, file, file_path, "com.oplus.camera.capture.hdr.support", "1")
 
+# Backs pref_heif_support_key: gates both the HEIF settings entry and the
+# capture path's heic_10bits choice. HEIC writes JPEG bytes into a .heic here.
+def set_photocodec_not_support(ctx, file, file_path, *args, **kwargs):
+    update_vendor_tag(ctx, file, file_path, "com.oplus.feature.photocodec.support", "0")
+
 blob_fixups: blob_fixups_user_type = {
     'odm/etc/init/init.camera_process.rc': blob_fixup()
         .regex_replace('    delete_recursion', '    #delete_recursion'),
@@ -247,7 +252,8 @@ blob_fixups: blob_fixups_user_type = {
         .call(set_video_4k_120fps_zoom_range)
         .call(set_video_4k120fps_max_zoom_list)
         .call(set_video_dv_120fps_support)
-        .call(set_camera_capture_hdr_support),
+        .call(set_camera_capture_hdr_support)
+        .call(set_photocodec_not_support),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

OP-13-dump/device_oneplus_dodge

Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

extract-files.py
@@ -120,6 +120,11 @@ def set_video_4k120fps_max_zoom_list(ctx, file, file_path, *args, **kwargs):
 def set_video_dv_120fps_support(ctx, file, file_path, *args, **kwargs):
     update_vendor_tag(ctx, file, file_path, "com.oplus.feature.video.dv.120fps.support", "1")
 
+# Backs pref_heif_support_key: gates both the HEIF settings entry and the
+# capture path's heic_10bits choice. HEIC writes JPEG bytes into a .heic here.
+def set_photocodec_not_support(ctx, file, file_path, *args, **kwargs):
+    update_vendor_tag(ctx, file, file_path, "com.oplus.feature.photocodec.support", "0")
+
 blob_fixups: blob_fixups_user_type = {
     'odm/etc/init/init.camera_process.rc': blob_fixup()
         .regex_replace('    delete_recursion', '    #delete_recursion'),
@@ -238,7 +243,8 @@ blob_fixups: blob_fixups_user_type = {
         .call(set_video_4k_120fps_support)
         .call(set_video_4k_120fps_zoom_range)
         .call(set_video_4k120fps_max_zoom_list)
-        .call(set_video_dv_120fps_support),
+        .call(set_video_dv_120fps_support)
+        .call(set_photocodec_not_support),
 }  # fmt: skip
 
 module = ExtractUtilsModule(